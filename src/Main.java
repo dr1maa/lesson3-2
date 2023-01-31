d5d262b10c08075b1dd0aa285042d443078eb293
aleksej <dr1ma@icloud.com>
2023-01-31 22:55:49 +0600

    исправил с выводом среднего арифметического

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-//Пусть дан произвольный список целых чисел, удалить из него четные числа
+//Задан целочисленный список ArrayList.
+// Найти минимальное, максимальное и среднее ариф. из этого списка
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,12 +24,13 @@ public class Main {
         }
         System.out.println("минимальное число: " + min);
         System.out.println("максимальное число: " + max);
-        int sum = 0;
+        double sum = 0;
 
         for (int i = 0; i < arr.size(); i++) {
             sum += arr.get(i);
         }
 
-        return sum / arr.size();
+         double result = sum / arr.size();
+        System.out.println("Спеднее арифметическое : " + result);
     }
 }
